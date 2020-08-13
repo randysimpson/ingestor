88b05b71ac1c818643fb33df9b064ad64d8d4f20
Randy Simpson <rsimpsoncode@gmail.com>
2020-08-13 13:57:34 -0600

    Added support for pg database

--- controller/saveData.js
+++ controller/saveData.js
@@ -1,12 +1,13 @@
 const MongoClient = require('mongodb').MongoClient;
+const { Pool } = require('pg');
 const config = require('../model/config');
 
-//save this data object into database.
-const saveData = (data) => {
+//save the object to mongo
+const saveMongo = (data) => {
   return new Promise((resolve, reject) => {
     // Use connect method to connect to the server
-    MongoClient.connect(config.dburl, function(err, client) {
-      if(err) {
+    MongoClient.connect(config.dburl, function (err, client) {
+      if (err) {
         return reject(err);
       }
       const db = client.db(config.database);
@@ -14,15 +15,52 @@ const saveData = (data) => {
       // Get the metrics collection
       const collection = db.collection('metrics');
       // Insert the data
-      collection.insertMany(data, function(err, result) {
-        if(err) {
+      collection.insertMany(data, function (err, result) {
+        if (err) {
           return reject(err);
         }
         resolve(result);
       });
       client.close();
     });
-  })
+  });
+};
+
+//save single metric to postgres
+const savePg = (pool, metric) => {
+  if(metric.tags) {
+    const sql = 'INSERT INTO metrics (date, metric, value, tags, source) VALUES ($1, $2, $3, $4, $5);';
+    const values = [metric.date, metric.metric, metric.value, metric.tags, metric.source];
+    return pool.query(sql, values);
+  } else {
+    const sql = 'INSERT INTO metrics (date, metric, value, source) VALUES ($1, $2, $3, $4);';
+    const values = [metric.date, metric.metric, metric.value, metric.source];
+    return pool.query(sql, values);
+  }
+}
+
+//save the object to postgres
+const saveMetricsPg = (data) => {
+  // setup the connection pool
+  const pool = new Pool({
+    user: config.dbuser,
+    host: config.dbhost,
+    database: config.dbname,
+    password: config.dbpass,
+    port: parseInt(config.dbport)
+  });
+
+  //run sequentially
+  return data.reduce((p, item) => p.then(_ => savePg(pool, item)), Promise.resolve());
+};
+
+//save this data object into database.
+const saveData = (data) => {
+  if (config.dbtype === 'pg') {
+    return saveMetricsPg(data);
+  } else {
+    return saveMongo(data);
+  }
 };
 
 module.exports = saveData;
